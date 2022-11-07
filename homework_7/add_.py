394ed422517b1484f96f5a5da9e9919abe3e6cdd
Tamaracode1387 <107562738+Tamaracode1387@users.noreply.github.com>
2022-11-07 23:14:43 +0700

    Delete homework_7 directory

--- homework_7/add_.py
+++
@@ -1,3 +0,0 @@
-def add_func(some_person):
-    with open('database.txt', 'a', encoding='utf-8') as f:
-        f.write(some_person + '\n')
--- homework_7/read_.py
+++
@@ -1,6 +0,0 @@
-def read_func():
-    with open('database.txt', 'r', encoding='utf-8') as f:
-        info_list = f.read().splitlines()
-        for line in info_list:
-            print(line)
-
